ae9edc30090a0735c0974f264962fed411369952
CHRISTOFFER CARLSSON <CHRISTOFFER.CARLSSON88@gmail.com>
2025-11-03 11:17:18 +0100

    start lab geometric oop utils first

---
+++ Lab2/utils.py
@@ -0,0 +1,15 @@
+from numbers import Number
+
+class Utils:
+    """
+    Validation for numeric numbers in this geometric lab.
+    Made to control that number is positive and non-zero.
+    """
+    def validate_number(self, value):
+        if not isinstance(value, Number):
+            raise TypeError(f"{value} is not a number.")
+
+    def validate_positive(self, value):
+        self.validate_number(value)
+        if value <= 0:
+            raise ValueError(f"{value} is not positive and non-zero.")
